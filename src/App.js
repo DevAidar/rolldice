33858032637e3de79f97d06560e789209caadccc
Lime Peach <limepeach@LimePeach.local>
2020-09-18 07:55:25 -0700

    [Add] Dice is rolling towards the middle [Add] Random velocity given to the die

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { softShadows } from 'drei';
+import { Physics, usePlane } from 'use-cannon';
+import { Vector2 } from 'three';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Dice from './components/Dice';
+
+import './App.scss';
+
+softShadows();
+
+function Plane(props) {
+	const [ref] = usePlane(() => ({ mass: 0, friction: 0.8, rotation: [-Math.PI / 2, 0, 0], ...props }));
+	return (
+		<mesh ref={ref} receiveShadow>
+			<planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+			<meshStandardMaterial attach="material" color="#EFDFD5" smoothShading/>
+		</mesh>
+	);
 }
+
+const Wall = ({ rotation, args, position }) => {
+	const [ref] = usePlane(() => ({ mass: 0, friction: 100, rotation: rotation, position: position }));
+  
+	return (
+		<mesh ref={ref}>
+			<planeBufferGeometry attach='geometry' args={args}/>
+			{/* <meshStandardMaterial attach="material" color="#FFFFFF"/> */}
+			<meshStandardMaterial attach="material" color="#FFFFFF" opacity={0} transparent={true}/>
+		</mesh>
+	);
+};
+
+const App = () => {
+	const VIEW_WIDTH = window.innerWidth / 100;
+	const CAMERA_HEIGHT = 100;
+	const [fov, setFov] = useState();
+
+	useEffect(() => setFov(2 * Math.atan( ( VIEW_WIDTH / (window.innerWidth / window.innerHeight) ) / ( 2 * CAMERA_HEIGHT ) ) * ( 180 / Math.PI )), [VIEW_WIDTH]);
+
+	return (
+		<div className="App">
+			<Canvas shadowMap colorManagement gl={{ alpha: false, antialias: true, logarithmicDepthBuffer: true }} camera={{ position: [0, CAMERA_HEIGHT, 0], fov: fov }}>
+				{/* <hemisphereLight intensity={0.35} /> */}
+				{/* <spotLight position={[5, 5, 5]} angle={0.3} penumbra={1} intensity={2} castShadow shadow-mapSize-width={256} shadow-mapSize-height={256} /> */}
+				<ambientLight color='#f0f5fb' intensity={.5}/>
+				{/* <pointLight
+					castShadow
+					intensity={2.0}
+					color={0xefdfd5}
+					position={[10, 50, 10]}
+					// shadow={{ radius: 8 }}
+				/> */}
+				{/* <spotLight
+					castShadow
+					intensity={0.25}
+					position={[window.innerWidth / 200 - .2, 50, -(window.innerHeight / 200 - .2)]}
+					angle={Math.PI / 3}
+					penumbra={1}
+					shadow-mapSize={new Vector2(2048 * 5, 2048 * 5)}
+				/> */}
+				{/* <spotLight
+					castShadow
+					intensity={0.25}
+					position={[-(window.innerWidth / 200 - .2), 50, window.innerHeight / 200 - .2]}
+					angle={Math.PI / 3}
+					penumbra={1}
+					shadow-mapSize={new Vector2(2048 * 5, 2048 * 5)}
+				/> */}
+				{/* <spotLight
+					castShadow
+					intensity={0.25}
+					position={[-(window.innerWidth / 200 - .2), 50, window.innerHeight / 200 - .2]}
+					angle={Math.PI / 3}
+					penumbra={1}
+					shadow-mapSize={new Vector2(2048 * 5, 2048 * 5)}
+				/> */}
+				{/* <spotLight
+					castShadow
+					intensity={0.25}
+					position={[window.innerWidth / 200 - .2, 50, -(window.innerHeight / 200 - .2)]}
+					angle={Math.PI / 3}
+					penumbra={1}
+					shadow-mapSize={new Vector2(2048 * 5, 2048 * 5)}
+				/> */}
+				<pointLight 
+					castShadow
+					intensity={0.8}
+					position={[window.innerWidth / 200 - .2, 50, -(window.innerHeight / 200 - .2)]}
+					angle={Math.PI / 3}
+					shadow-mapSize={new Vector2(2048 * 5, 2048 * 5)}
+				/>
+				<Physics gravity={[0, -30, 0]}>
+					<Plane/>
+					<Wall rotation={[0, 0, 0]} args={[window.innerWidth / 100 - .4, 100, 0]} position={[0, 50, -(window.innerHeight / 200 - .2)]}/>
+					<Wall rotation={[0, -Math.PI / 2, 0]} args={[window.innerHeight / 100 - .4, 100, 0]} position={[window.innerWidth / 200 - .2, 50, 0]}/>
+					<Wall rotation={[0, Math.PI, 0]} args={[window.innerWidth / 100 - .4, 100, 0]} position={[0, 50, window.innerHeight / 200 - .2]}/>
+					<Wall rotation={[0, Math.PI / 2, 0]} args={[window.innerHeight / 100 - .4, 100, 0]} position={[-(window.innerWidth / 200 - .2), 50, 0]}/>
+					<Dice/>
+					{/* <Dice position={[window.innerWidth / 200 - 1, 1, window.innerHeight / 200 - 1]}/> */}
+					{/* <group>
+						<mesh
+							rotation={[-Math.PI / 2, 0, 0]}
+							position={[0, -3, 0]}
+							receiveShadow
+						>
+							<planeBufferGeometry attach='geometry' args={[100, 100]} />
+							<shadowMaterial attach='material' opacity={0.3} />
+						</mesh>
+						<SpinningMesh
+							position={[-2, 1, -5]}
+							color='pink'
+							speed={6}
+						/>
+						<RoundedBox
+							args={[1, 1, 1]}  // Width, Height and Depth of the box
+							radius={.1}     // Border-Radius of the box
+							smoothness={4}    // Optional, number of subdivisions
+						// {...meshProps}    // All THREE.Mesh props are valid
+						>
+							<meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
+						</RoundedBox>
+					</group> */}
+				</Physics>
+				{/* Camera Movements */}
+				{/* <OrbitControls /> */}
+			</Canvas>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Dice.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { useBox } from 'use-cannon';
+import { CanvasTexture, Vector3 } from 'three';
+
+import image1 from '../images/RoundIcons-Free-Set-01.png';
+import image2 from '../images/RoundIcons-Free-Set-02.png';
+import image3 from '../images/RoundIcons-Free-Set-03.png';
+import image4 from '../images/RoundIcons-Free-Set-04.png';
+import image5 from '../images/RoundIcons-Free-Set-05.png';
+import image6 from '../images/RoundIcons-Free-Set-06.png';
+import image7 from '../images/RoundIcons-Free-Set-07.png';
+import image8 from '../images/RoundIcons-Free-Set-08.png';
+import image9 from '../images/RoundIcons-Free-Set-09.png';
+import image10 from '../images/RoundIcons-Free-Set-10.png';
+import image11 from '../images/RoundIcons-Free-Set-11.png';
+
+const Dice = ({ position }) => {
+	
+	const [ref, api] = useBox(() => {
+		switch (parseInt(Math.random() * 4 + 1)) {
+		case 1: 
+			position = [((Math.random() * 2) - 1) * (window.innerWidth / 200 - 1), 10, -(window.innerHeight / 200 - 1)];
+			break;
+		case 2: 
+			position = [window.innerWidth / 200 - 1, 10, ((Math.random() * 2) - 1) * (window.innerHeight / 200 - 1)];
+			break;
+		case 3: 
+			position = [((Math.random() * 2) - 1) * (window.innerWidth / 200 - 1), 10, window.innerHeight / 200 - 1];
+			break;
+		case 4: 
+			position = [Math.random() * (window.innerWidth / 200), 10, window.innerHeight / 200 - 1];
+			break;
+		default: 
+			position = [((Math.random() * 2) - 1) * (window.innerWidth / 200) , 10, window.innerHeight / 200 - 1];
+			break;
+		}
+  
+		return {
+			mass: 300,
+			args: [1, 1, 1],
+			position: position,
+			friction: 0.8, 
+		};});
+
+	const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11];
+	const [textures, setTextures] = useState([]);
+  
+	const throwDice = useCallback(() => {
+		api.applyImpulse([0,10000,0],[Math.random() - .5,-.5,Math.random() - .5]);
+		api.velocity.set(-(ref.current.position.x) * (Math.random() * 10 + 5), 10, -(ref.current.position.z) * (Math.random() * 10 + 5));
+	}, [api, ref]);
+  
+	useEffect(() => {
+		if (!textures.length) {
+			throwDice();
+		}
+	}, [textures, throwDice]);
+
+	useEffect(() => {
+		if (textures.length < 6) {
+			let img = new Image();
+			img.src = images[textures.length];
+			img.onload = () => {
+				let ctx = document.createElement('canvas').getContext('2d');
+				ctx.canvas.width = 512;
+				ctx.canvas.height = 512;
+
+				ctx.beginPath();
+				ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
+				ctx.fillStyle = '#636e72';
+				ctx.fill();
+				ctx.closePath();
+
+				ctx.beginPath();
+				ctx.drawImage(
+					img,
+					0,
+					0,
+					img.width,
+					img.height,
+					ctx.canvas.width * .05,
+					ctx.canvas.height * .05,
+					ctx.canvas.width * .9,
+					ctx.canvas.height * .9,
+				);
+				ctx.closePath();
+
+				setTextures([...textures, new CanvasTexture(ctx.canvas)]);
+			};
+		}
+	}, [images, textures]);
+  
+	return <mesh
+		receiveShadow
+		castShadow
+		ref={ref}
+		onClick={() => throwDice()}
+	>
+		{textures.map((texture, index) => (
+			<meshStandardMaterial
+				receiveShadow
+				attachArray='material'
+				map={texture}
+				key={index}
+				smoothShading
+			/>
+		))}
+		<boxBufferGeometry attach="geometry" />
+	</mesh>;
+};
+
+export default Dice;
