09ee4af9fc1b7419f011851383c5355e3a7aefcd
Lime Peach <limepeach@LimePeach.local>
2020-09-28 07:00:08 -0700

    [Delete] Temporarily removed Helmet

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -16,10 +16,23 @@ const uploadsPath = path.join(__dirname, '..', 'uploads');
 const port = process.env.PORT || 3000;
 
 app.use(morgan('common'));
-app.use(helmet());
+// app.use(
+// 	helmet({
+// 		expectCt: false,
+// 	}),
+// );
+// app.use(
+// 	helmet.contentSecurityPolicy({
+// 		directives: {
+// 			'default-src': ["'self'"],
+// 			'script-src': ["'self'", "'unsafe-inline'", 'code.jquery.com', 'cdn.jsdelivr.net', 'stackpath.bootstrapcdn.com'],
+// 			'style-src': ["'self'", 'cdnjs.cloudflare.com', 'stackpath.bootstrapcdn.com'],
+// 		},
+// 	}),
+// );
 app.use(cors({
 	origin: process.env.CORS_ORIGIN,
-	// exposedHeaders: 'auth-token',
+	exposedHeaders: 'auth-token',
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,7 +41,7 @@ app.use('/api', apiRouter);
 app.use('/uploads', express.static(uploadsPath));
 
 app.use(express.static(publicPath));
-app.use((_, res) => {
+app.use('*', (_, res) => {
 	res.sendFile(path.join(publicPath, 'index.html'));
 });
 
